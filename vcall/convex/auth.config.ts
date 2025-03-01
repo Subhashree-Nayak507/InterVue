c4fcad809203781cce42af56aa595127443ca3e0
Subhashree-Nayak507 <kukisubhashree@gmail.com>
2025-03-01 18:41:25 +0530

    setup and auth part completed

---
+++ vcall/convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://pro-jackal-23.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ vcall/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+<div className="m-10">
+      <SignedOut>
+              <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+             <UserButton />
+      </SignedIn>
+</div>
+  );
+}
